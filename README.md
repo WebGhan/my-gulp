60d65b4ccab0fba1c688a7c6497ecab02626fa7b
Gao Han <869912332@qq.com>
2019-10-09 15:41:00 +0800

    修改目录结构

---
+++ README.md
@@ -0,0 +1,25 @@
+# 配置gulp
+
+```
+ npm install
+```
+
+### 目录结构
+```
+├── config
+├── dist
+│   ├── css
+│   └── js
+├── src
+│   ├── js
+│   ├── css
+│   ├── scss
+│   └── less
+├── index.html
+├── gulpfile.js
+└── package.json
+```
+
+### 如何使用gulp？
+
+文档：[https://www.gulpjs.com.cn](https://www.gulpjs.com.cn/)
--- gulpfile.js
+++ gulpfile.js
@@ -9,40 +9,40 @@ sass.compiler = require('node-sass');
 
 // 压缩js
 function compressJs() {
-  return src('js/**/*.js')
+  return src('src/js/**/*.js')
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
     .pipe(dest('dist/js/'));
 }
-watch('js/**/*.js', compressJs)
+watch('src/js/**/*.js', compressJs)
 
 // 压缩css
 function compressCss() {
-  return src('css/**/*.css')
+  return src('src/css/**/*.css')
     .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest('dist/css/'));
 }
-watch('css/**/*.css', compressCss)
+watch('src/css/**/*.css', compressCss)
 
 // 处理scss
 function handleScss() {
-  return src('scss/**/*.scss')
-    // 可设置输出格式 CSS output style (nested | expanded | compact | compressed)
+  return src('src/scss/**/*.scss')
+    // 可设置输出格式 (nested | expanded | compact | compressed)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest('dist/css/'));
 }
-watch('scss/**/*.scss', handleScss)
+watch('src/scss/**/*.scss', handleScss)
 
 // 处理less
 function handleLess() {
-  return src('less/**/*.less')
+  return src('src/less/**/*.less')
     .pipe(less())
     .pipe(cleanCSS())
     .pipe(rename({ extname: '.min.css' }))
     .pipe(dest('dist/css/'));
 }
-watch('less/**/*.less', handleLess)
+watch('src/less/**/*.less', handleLess)
 
 exports.build = series(compressJs, compressCss, handleScss, handleLess);
